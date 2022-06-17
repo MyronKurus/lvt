744f81f53e4125f7d25c2f85b2f6aa17ef30df82
Myron Kurus <myron.kurus@gmail.com>
2022-06-18 01:01:09 +0300

    Fix > date formatter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {MAT_DATE_FORMATS} from '@angular/material/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 
 import {HttpLoaderFactory} from "./core/helpers/http-loader-factory";
-import {DATE_FORMATS} from "./core/helpers/datepicker-formatter";
 
 import {AppRoutingModule} from "./app-routing.module";
 import {DialogModule} from "./shared/dialogs/dialog.module";
@@ -32,12 +30,6 @@ import {AppComponent} from './app.component';
       },
       useDefaultLang: false
     }),
-  ],
-  providers: [
-    {
-      provide: MAT_DATE_FORMATS,
-      useValue: DATE_FORMATS
-    }
   ],
   bootstrap: [AppComponent]
 })
