0ff2f1db0f9809bf6dbdf23bf05e4543c8b0df91
Myron Kurus <myron.kurus@gmail.com>
2022-06-20 23:53:21 +0300

    Feature > status line / checkbox list

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,7 +10,7 @@ import {MaterialModule} from "./material/material.module";
 import {ChartComponent} from './components/chart/chart.component';
 import {SelectionFiltersComponent} from "./components/selection-filters/selection-filters.component";
 import {IndexSelectionComponent} from "./components/index-selection/index-selection.component";
-import { SummaryLineComponent } from './components/summary-line/summary-line.component';
+import {SummaryLineComponent} from './components/summary-line/summary-line.component';
 
 const MODULES = [
   CommonModule,
@@ -25,13 +25,13 @@ const COMPONENTS = [
   SelectionFiltersComponent,
   ChartComponent,
   IndexSelectionComponent,
+  SummaryLineComponent,
 ];
 
 
 @NgModule({
   declarations: [
     ...COMPONENTS,
-    SummaryLineComponent
   ],
   imports: [
     ...MODULES
@@ -39,7 +39,6 @@ const COMPONENTS = [
   exports: [
     ...MODULES,
     ...COMPONENTS,
-    SummaryLineComponent
   ]
 })
 export class SharedModule {
